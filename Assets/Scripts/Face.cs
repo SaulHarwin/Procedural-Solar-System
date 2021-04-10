ab817fa74097874fb1c6aa293034634a2cebe0d0
SaulHarwin <15swharwin@uckfield.college>
2021-04-10 15:36:39 +0100

    Procedural Planets

---
+++ Assets/Scripts/Face.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
+public class Face : MonoBehaviour {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles; 
+    public int chunkSize;
+
+    Vector3[] heightMap;
+
+    private void Awake() {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+    
+    private void Start() {
+        MakePlanet();
+        UpdateMesh();
+    }
+
+    private void MakePlanet() {
+
+        // TerrainGeneration();
+
+        // for (int i = 0; i < 6; i++) {
+            int i = 0;
+            MakeFace(i);
+        // }
+    }
+
+
+    private void MakeFace(int dir) {
+        vertices  = TerrainGeneration();
+        triangles = GenerateTriangles();
+    }
+
+    private Vector3[] TerrainGeneration() {
+        heightMap = new Vector3[((chunkSize)+1) * ((chunkSize)+1)];
+        int i = 0;
+        for (int x = 0; x <= chunkSize; x ++) {
+            for (int z = 0; z <= chunkSize; z ++) {
+                heightMap[i] = new Vector3(x, 0, z);
+                i ++;
+            }
+        }
+        return heightMap;
+    }
+
+    private void PivotFaces(Vector3[] face) {
+        Quaternion rotation = Quaternion.Euler(0, 0, 90);
+        for (int i = 0; i < face.Length; i++) {
+            face[i] = rotation * (face[i] - new Vector3(0,0,0)) + new Vector3(0,0,0);
+        }            
+    }
+
+    private int[] GenerateTriangles() {
+        triangles = new int[(chunkSize) * (chunkSize) * 6];
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < chunkSize; z ++) {
+            for (int x = 0; x < chunkSize; x ++) {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + (chunkSize) + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + (chunkSize) + 1;
+                triangles[tris + 5] = vert + (chunkSize) + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        };
+        return triangles;
+    }
+
+    private void UpdateMesh() {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace{
+
+    Noise noise = new Noise();
+    planetData planetData;
+
+    Mesh mesh;
+    Vector3 localUp; // Which way the planet is facing 
+    Vector3 axisA;   // Perpendicular to localUp
+    Vector3 axisB;   // Perpendicular to localUp and axisA
+
+    int resolution;
+    Vector3[] vertices;
+    int[] triangles;
+
+    public TerrainFace(Mesh mesh, Vector3 localUp, planetData planetData) {
+        this.planetData = planetData;
+        this.resolution = planetData.resolution;
+        this.mesh = mesh;
+        this.localUp = localUp;
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    public void ConstructMesh(Vector2 offset) {
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+        int triIndex = 0;
+        for (int y = 0; y < resolution; y++) {
+            for (int x = 0; x < resolution; x++) {
+                int i = x + y * resolution;
+                Sphere(vertices, i, x, y);
+                triIndex = Triangles(triangles, triIndex, i, x, y);
+            }
+        }
+        UpdateMesh(vertices, triangles);
+    }
+
+    private float SebNoise(Vector3 point) {
+        float noiseValue = 0;
+        float frequency = planetData.frequency;
+        float amplitude = planetData.amplitude;
+        for (int o = 0; o < planetData.octaves; o++, amplitude *= planetData.persistance, frequency *= planetData.lacinarity) {
+            noiseValue += ((noise.Evaluate(point * frequency) + 1) * .5f) * amplitude;
+        }
+        return noiseValue;
+    }
+
+    private void Sphere(Vector3[] vertices, int i, int x, int y) {
+        Vector2 percent = new Vector2(x, y) / (resolution - 1); 
+        Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+        Vector3 pointOnUnitSphere = pointOnUnitCube.normalized * planetData.radius;
+        pointOnUnitSphere *= (1 + SebNoise(pointOnUnitSphere));
+        vertices[i] = pointOnUnitSphere;
+    }
+
+    private int Triangles(int[] triangles, int triIndex, int i, int x, int y) {
+        if (x != resolution - 1 && y != resolution - 1) {
+            triangles[triIndex] = i;
+            triangles[triIndex + 1] = i + resolution + 1;
+            triangles[triIndex + 2] = i + resolution;
+            triangles[triIndex + 3] = i;
+            triangles[triIndex + 4] = i + 1;
+            triangles[triIndex + 5] = i + resolution + 1;
+            triIndex += 6;
+        }
+        return triIndex;
+    }
+
+    private void UpdateMesh(Vector3[] vertices, int[] triangles) {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
+
+public struct planetData {
+    public int resolution;
+    public float radius;
+    public float amplitude;
+    public float frequency;
+    public int octaves;
+    public float lacinarity;
+    public float persistance;
+}
